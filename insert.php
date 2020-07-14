a9d4dd5e481e8a43b4b52bf1948187650d938269
abdulsalamcodes <59220264+abdulsalamcodes@users.noreply.github.com>
2020-07-14 18:29:41 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -7,7 +7,7 @@
     if(!empty($username) || !empty($email) ) {
         $host = "qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         $dbUsername = "obuaewm0590j8zbw";
-        $dbPassword = "ewvvbfn1qn41a74g";
+        $dbPassword = "f0ckhi37dpxqun36";
         $dbname = "meapp";
 
         // create a connection
